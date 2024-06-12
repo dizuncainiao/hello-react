80d994c863fee51047e18befd704dfff99741bd1
wangbo <781278657@qq.com>
2024-06-12 15:37:52 +0800

    feat: 🎸 UseReducerDemo.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import MyTicTacToe from "./views/my-tic-tac-toe/MyTicTacToe.tsx";
-import UseStateDemo from "./views/use-state-demo/UseStateDemo.tsx";
 import "./index.css";
+// import MyTicTacToe from "./views/my-tic-tac-toe/MyTicTacToe.tsx";
+// import UseStateDemo from "./views/use-state-demo/UseStateDemo.tsx";
+import Counter from "./views/use-reducer-demo/UseReducerDemo.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UseStateDemo />
+    <Counter />
   </React.StrictMode>,
 );
---
+++ src/views/use-reducer-demo/UseReducerDemo.tsx
@@ -0,0 +1,27 @@
+import { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+function reducer(state: { count: number }, type: string) {
+  switch (type) {
+    case "add":
+      return { count: state.count + 1 };
+    case "sub":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+export default function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <p>当前计数：{state.count}</p>
+      <button onClick={() => dispatch("add")}>click to + 1</button> <button onClick={() => dispatch("sub")}>click to - 1</button>
+    </div>
+  );
+}
